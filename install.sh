3508585481bdbd81a54a6b53382ec07ec27d5a40
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-24 16:22:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/zfile-on-replit"
 git clone https://github.com/sxbai/zfile-on-replit.git
 mv zfile-on-replit/* . && cp -r zfile-on-replit/.replit .replit
+rm -rf zfile-on-replit/
 echo "搭建完成，使用愉快！！！"
-bash main.sh && rm install.sh
+echo "点击Run按钮启动项目！！！"
+rm -rf install.sh
