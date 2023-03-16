3b7925d392fdc097a6b06136620b08a1fa0dd317
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-16 21:49:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 echo "ZFile-On-Replit一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/zfile-on-replit"
-git clone https://github.com/sxbai/zfile-on-replit.git
-mv zfile-on-replit/* . && cp -r zfile-on-replit/.replit .replit
-rm -rf zfile-on-replit/
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/zfile-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/zfile-on-replit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/zfile-on-replit/raw/master/main.sh
+wget https://github.com/sxbai/zfile-on-replit/raw/master/application.properties
+cp -r build/.replit . && cp -r build/replit.nix .
 echo "搭建完成，使用愉快！！！"
 echo "点击Run按钮启动项目！！！"
-rm -rf install.sh
+rm -rf build/
