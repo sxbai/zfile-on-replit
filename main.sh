544128cf4f3c43c32b5478e9b3f41600b0f701e8
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-16 22:02:21 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 tag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/zfile-dev/zfile/releases/latest" | jq -r '.tag_name')
 wget -O zfile-${tag}.jar https://github.com/zfile-dev/zfile/releases/download/${tag}/zfile-${tag}.jar \
-&& java -jar -Duser.timezone=Asia/Shanghai zfile-${tag}.jar --spring.config.location=file:application.properties
+&& java -jar -Duser.timezone=Asia/Shanghai zfile-${tag}.jar --zfile.log.path=/home/runner/${REPL_SLUG}/.zfile-v4/logs --zfile.db.path=/home/runner/${REPL_SLUG}/.zfile-v4/db/zfile
